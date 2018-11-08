2a79b4727ad51ce546daf8767e382afa03f9e44f
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-08 06:16:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 function printBadges(array) {
   for(let i=0; i<array.length; ++i) {
-    console.log('Welcome ${array[i]}! You are employee #${i + 1}.');
+    console.log(`Welcome ${array[i]}! You are employee #${i + 1}.`);
+  //  console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
   }
   return array;
 }
