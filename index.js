8b9ef7162b50f396556465c6b99a1e0b2e1e6811
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-08 06:12:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Code your solutions in this file
 
 function printBadges(array) {
-
+  for(let i=0; i<array.length; ++i) {
+    console.log('Welcome ${array[i]}! You are employee #${i+1}');
+  }
+  return array
 }
